7aea39e69cee000d7554b4588c3de5edc424af3e
Eric Napier <napieed@gmail.com>
2018-12-15 16:44:15 -0600

    Adding initial ef migration. Adding some configuration.

--- DutchTreat/Program.cs
+++ DutchTreat/Program.cs
@@ -19,6 +19,18 @@ namespace DutchTreat
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
                 .UseStartup<Startup>();
+
+        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            builder.Sources.Clear();
+
+            ///This is a cool way to do configuration. The json file is good for development time,
+            ///but it can be superseded by the environment variables in production. This way,
+            ///I can deploy to production without editing any config files.
+            builder.AddJsonFile("config.json", false, true)
+                   .AddEnvironmentVariables();
+        }
     }
 }
